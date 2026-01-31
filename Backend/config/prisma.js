04de9136a8818898e3fa0f4c2d41ded5f7635292
Meet-08 <meetjbhuva@gmail.com>
2026-01-31 15:12:28 +0530

    fix: DB

--- Backend/config/prisma.js
+++ Backend/config/prisma.js
@@ -1,10 +1,10 @@
-import { PrismaMssql } from "@prisma/adapter-mssql";
+import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/index.js";
 
 const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaMssql({ connectionString });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
